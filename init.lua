0fdef2f5cabc1ecb1992dd4af1f04a0035d49cbe
Rajesh Pandey <rajesh@MacBookPro.lan>
2025-05-19 12:02:24 +0530

    chore:init nvim config

---
+++ init.lua
@@ -0,0 +1,48 @@
+require("core.keymaps")
+require("core.options")
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close he window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require("lazy").setup({
+	-- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+	require("plugins.colortheme"),
+	require("plugins.neotree"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocomplete"),
+	require("plugins.none-ls"),
+	require("plugins.gitsigns"),
+	require("plugins.alpha"),
+	require("plugins.indent-blankline"),
+	require("plugins.misc"),
+	require("plugins.comment"),
+})
+vim.cmd("colorscheme cyberdream")
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
